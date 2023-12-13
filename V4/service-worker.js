15b9ae64f8a9533649c49ef94567d0085d147d7b
Mr-funkinguy <balopez1210@gmail.com>
2023-12-12 23:37:04 -0800

    i

---
+++ V4/service-worker.js
@@ -0,0 +1,38 @@
+
+
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.0.2/workbox-sw.js');
+
+workbox.routing.registerRoute(
+    ({request}) => request.destination === 'image',
+    new workbox.strategies.NetworkFirst()
+);
+
+const cacheName = 'Sz-Games';
+// List the files to precache
+const precacheResources = ['/',
+ '/index.html',
+];
+
+// When the service worker is installing, open the cache and add the precache resources to it
+self.addEventListener('install', (event) => {
+  console.log('Service worker install event!');
+  event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(precacheResources)));
+
+});
+
+self.addEventListener('activate', (event) => {
+  console.log('Service worker activate event!');
+});
+
+
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+      caches.match(event.request).then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+    );
+  });
